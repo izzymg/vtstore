73585c75c34a4fef5c16cbbd314294ae69f43dfd
izzymg <izzymhg@gmail.com>
2021-07-15 09:39:25 +1200

    Type returns, use Record<> not Object

--- lib/vtstore.ts
+++ lib/vtstore.ts
@@ -1,21 +1,21 @@
-import { reactive, readonly, toRefs, } from "vue"
+import { DeepReadonly, reactive, readonly, ToRefs, toRefs, } from "vue"
 
-class VTStore<T extends Object> {
+class VTStore<T extends Record<string, unknown>> {
     private underlying: T
 
     constructor(initial: T) {
         this.underlying = reactive(initial) as T
     }
 
-    public mutate<K extends keyof T>(key: K, val: T[K]) {
+    public mutate<K extends keyof T>(key: K, val: T[K]): void {
         this.underlying[key] = val
     }
 
-    get state() {
-        return readonly(this.underlying)
+    get state(): DeepReadonly<T> {
+        return readonly(this.underlying) as DeepReadonly<T>
     }
 
-    get refs() {
+    get refs(): ToRefs<DeepReadonly<T>> {
         return toRefs(this.state)
     }
 }
